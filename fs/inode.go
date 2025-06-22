699f57477e98b2bc3647efb17de989fd81271b6a
kche0169 <shiratakekanpakuji@gmail.com>
2025-06-22 23:32:45 +0800

    Feat: Add mode enum

--- fs/inode.go
+++ fs/inode.go
@@ -12,13 +12,21 @@ const (
 	Directory
 )
 
+type mode uint32
+
+const (
+	ModeExecute mode = 1 << iota
+	ModeWrite
+	ModeRead
+)
+
 // Inode 结构体定义文件/目录元数据
 type Inode struct {
 	ID         uint64    // 唯一标识
 	Name       string    // 文件或目录名
 	Type       FileType  // 文件 or 目录
 	Size       uint64    // 文件大小（字节，目录为0或统计所有子项）
-	Mode       uint32    // 权限，如Unix风格
+	Mode       mode      // 权限，如Unix风格
 	Owner      string    // 所有者
 	Group      string    // 所属组
 	CreatedAt  time.Time // 创建时间
@@ -32,7 +40,7 @@ type Inode struct {
 }
 
 // NewFileInode 创建文件类型的inode
-func NewFileInode(id uint64, name, owner, group string, mode uint32) *Inode {
+func NewFileInode(id uint64, name, owner, group string, mode mode) *Inode {
 	now := time.Now()
 	return &Inode{
 		ID:         id,
@@ -49,7 +57,7 @@ func NewFileInode(id uint64, name, owner, group string, mode uint32) *Inode {
 }
 
 // NewDirInode 创建目录类型的inode
-func NewDirInode(id uint64, name, owner, group string, mode uint32) *Inode {
+func NewDirInode(id uint64, name, owner, group string, mode mode) *Inode {
 	now := time.Now()
 	return &Inode{
 		ID:         id,
@@ -73,4 +81,19 @@ func (i *Inode) IsDir() bool {
 // IsFile 判断是否为文件
 func (i *Inode) IsFile() bool {
 	return i.Type == File
+}
+
+// CanRead 判断是否可读
+func (i *Inode) CanRead() bool {
+	return i.Mode&ModeRead != 0
+}
+
+// CanWrite 判断是否可写
+func (i *Inode) CanWrite() bool {
+	return i.Mode&ModeWrite != 0
+}
+
+// CanExecute 判断是否可执行
+func (i *Inode) CanExecute() bool {
+	return i.Mode&ModeExecute != 0
 }
